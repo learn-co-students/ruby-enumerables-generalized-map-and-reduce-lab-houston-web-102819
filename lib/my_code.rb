6b9d997bd4688f1e1900f64557cc04cf7ca8ed05
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 14:19:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  counter = 0
+  while source_array[counter] do
+    new_array << yield(source_array[counter])
+    counter += 1
+  end
+  return new_array
+end
+
+def reduce(elements, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
+end
